beaf8c3c0e3e6b596ed0ffb795c1f518222e814e
balha123 <bensaad.hedi@esprit.tn>
2026-02-07 02:00:46 +0100

    Ajout de la base de données pour Vercel

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,17 @@
 import { createClient } from "@libsql/client";
 
+const url = process.env.TURSO_DATABASE_URL;
+const authToken = process.env.TURSO_AUTH_TOKEN;
+
+if (!url) {
+  throw new Error(
+    "🚨 ERREUR CRITIQUE : La variable d'environnement 'TURSO_DATABASE_URL' est manquante. Vérifiez votre fichier .env.local ou les réglages Vercel.",
+  );
+}
+
 const db = createClient({
-  url: process.env.TURSO_DATABASE_URL!,
-  authToken: process.env.TURSO_AUTH_TOKEN!,
+  url,
+  authToken,
 });
 
 export default db;
